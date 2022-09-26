a434dd5c93c5349dd03e76549eceb8441f7bc7a0
Santiago Serpa <ss5gc4@gmail.com>
2022-09-25 19:48:29 -0500

    Add ok

--- js/login.js
+++ js/login.js
@@ -1,4 +1,4 @@
-const loginUrl = "https://mintic-bancoproj-g2.herokuapp.com/biciapp/login";
+const loginUrl = "https://bicimarketunal.herokuapp.com/biciapp/login";
 //const loginUrl = "http://127.0.0.1:8000/biciapp/login";
 function collectData(evt){
     evt.preventDefault();
